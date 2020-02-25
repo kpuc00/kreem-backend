d102d2074962a0e45842f4c767798c50800b7ef4
Mihail Bondarenco <441044@student.fontys.nl>
2020-02-25 01:23:17 +0100

    feat(login) implement login functionality into blank main window

---
+++ KreemMachine/KreemMachineLibrary/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using KreemMachineLibrary.Helpers;
+using KreemMachineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreemMachineLibrary.Services
+{
+    public class UserService
+    {
+
+        DataBaseContext db = Globals.db;
+        
+        internal User Create(string name, string email, Role role, string password) {   
+            string hash = PasswordHashHelper.CreateHash(password);
+            var user = new User(name, email, role, hash);
+            return user;
+        } 
+
+        public User CreateAndSave(string name, string email, Role role, string password)
+        {
+            var user = Create(name, email, role, password);
+
+            user = SaveToDatabase(user);
+            
+       
+            db.SaveChanges();
+            return user;
+        }
+
+        internal User SaveToDatabase (User user)
+        {
+            db.Roles.Attach(user.Role);
+            return db.Users.Add(user);
+        }
+
+        /// <summary>
+        /// Returns the user from the database if the password matches the hash in the database
+        /// </summary>
+        /// <param name="email">user's email</param>
+        /// <param name="password">the password entered by the user</param>
+        /// <returns> the user identified by the <code>email</code> and <code>password</code> 
+        /// or <code>null</code> if credentials wrong</returns>
+        public User getByCredentials (string email, string password)
+        {
+            var candidate = db.Users.SingleOrDefault(u => u.Email == email);
+            if (candidate == null)
+                return null;
+
+            if (PasswordHashHelper.VerifyPassword(password, candidate.Hash))
+                return candidate;
+
+            return null;
+        }
+
+
+    }
+}
--- KreemMachine/KreemMachineUI/LoginWindow.xaml.cs
+++ KreemMachine/KreemMachineUI/LoginWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,22 +13,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KreemMachineLibrary.Models;
+using KreemMachineLibrary.Services;
 
 namespace KreemMachine
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+
+        UserService users;
+
+        public LoginWindow()
         {
             InitializeComponent();
+
+            // Run initialization on separate thread so that the UI doesn't hang 
+            new Thread( () => users = new UserService() ).Start();
         }
+
+        string Email => EmailTextBox.Text;
+
+        string Password => SecretPasswordBox.Password;
 
         private void LogInButton_Click(object sender, RoutedEventArgs e)
         {
 
+            //If the thread initializing users hasn't yet finished
+            //users might still be null, hence safe navigation operator ?.
+            var user = users?.getByCredentials(Email, Password);
+
+            if (user != null)
+            {
+                var window = new MainWindow(user);
+                window.Show();
+                
+                this.Hide();
+                // after 'MainWindow' is closed, also close This Window
+                window.Closed += (Sender, E) => this.Close();
+                
+            }
+            else
+            {
+                MessageBox.Show("Either email or password is wrong");
+            }
         }
     }
 }
