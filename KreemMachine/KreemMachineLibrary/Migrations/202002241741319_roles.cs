8b8e3b4e8ee359b6e75a6d3c964b24897b529d37
Mihail Bondarenco <441044@student.fontys.nl>
2020-02-25 01:18:16 +0100

    feat(db) add two db migrations

---
+++ KreemMachine/KreemMachineLibrary/Migrations/202002241741319_roles.cs
@@ -0,0 +1,26 @@
+﻿namespace KreemMachineLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class roles : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(nullable: false, unicode: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Roles");
+        }
+    }
+}
---
+++ KreemMachine/KreemMachineLibrary/Migrations/Configuration.cs
@@ -0,0 +1,39 @@
+﻿namespace KreemMachineLibrary.Migrations
+{
+    using KreemMachineLibrary.Models;
+    using KreemMachineLibrary.Services;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<KreemMachineLibrary.DataBaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
+        }
+
+        /// <summary>
+        /// Is called after every migration.
+        /// In case there is no data in the database it will create some bare-minimum entries
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Seed(DataBaseContext context)
+        {
+            context.Roles.AddOrUpdate(
+                new Role(1, nameof(RolesService.Administrator)),
+                new Role(2, nameof(RolesService.Manager)),
+                new Role(3, nameof(RolesService.Depot)),
+                new Role(4, nameof(RolesService.Employee))
+            );
+            var userService = new UserService();
+            var user = userService.Create("Admin Adminov", "admin@mail.com", null, "qweqwe");
+            user.role = 1;
+            user.Id = 1;
+            context.Users.AddOrUpdate(user);
+
+        }
+    }
+}
