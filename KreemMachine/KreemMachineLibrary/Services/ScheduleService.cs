19544a91b9701674ba7155ac9f9faf743ad81039
Mihail Bondarenco <441044@student.fontys.nl>
2020-03-12 20:02:02 +0100

    feat(schedule) rework shift to calendar matching alghorythm

--- KreemMachine/KreemMachineLibrary/Services/ScheduleService.cs
+++ KreemMachine/KreemMachineLibrary/Services/ScheduleService.cs
@@ -11,10 +11,17 @@ namespace KreemMachineLibrary.Services
     {
         private DataBaseContext db = Globals.db;
 
+        /// <summary>
+        /// Returns all shifts scheduled between <code>start</code> and <code>end</code> dates
+        /// Clients may depend on the fact that the result is sorted ascending by date
+        /// </summary>
+        /// <param name="start"> The first date to be included in the results (i.e. incluseive bound) </param>
+        /// <param name="end"> The first date that cannot be included in the results (i.e. exclusive bound)</param>
+        /// <returns>Returns all shifts scheduled between <code>start</code> and <code>end</code> dates</returns>
         public IEnumerable<ScheduledShift> GetScheduledShifts(DateTime start, DateTime end)
         {
             var shifts = db.ScheduledShifts
-                            .Where(s => s.Date > start && s.Date < end)
+                            .Where(s => s.Date >= start && s.Date < end)
                             .OrderBy(s => s.Date);
 
 
--- KreemMachine/KreemMachineUI/MainWindow.xaml.cs
+++ KreemMachine/KreemMachineUI/MainWindow.xaml.cs
@@ -58,50 +58,57 @@ namespace KreemMachine
 
         #region Schedule
 
-        private void ScheduleMonthPicker_SelectedMonthChanged(object sender, DateTime month)
+        private void ScheduleMonthPicker_SelectedMonthChanged(object sender, DateTime displayMonth)
         {
 
-            var days = new List<ScheduleDayViewModel>();
+            var calendarDays = new List<ScheduleDayViewModel>();
 
-            // DayOfWeek returns 0 for sunday, 1 for monday, 2 for toesdat and so on
-            int skips = (int)month.DayOfWeek - 1;
-            skips = (skips == -1) ? 6 : skips;
-
-            for (int i = 0; i < skips; i++)
-                days.Add(null);
+            addNullPaddingToCallendarDays();
+            addMonthDaysToCallendarDays();
+            matchScheduledShiftsToCallendarDays();
             
+            ScheduleListBox.ItemsSource = calendarDays;
 
-            for (var day = month; day < month.AddMonths(1); day = day.AddDays(1))
-                days.Add(new ScheduleDayViewModel(day));
-
-            IEnumerable<ScheduledShift> shifts = scheduleService.GetScheduledShifts(start: month, end: month.AddMonths(1));
-
-            IEnumerator<ScheduleDayViewModel> enumerator = days.GetEnumerator();
-            
-            while(enumerator.Current == null)
-                enumerator.MoveNext();
-
-
-            foreach(var shift in shifts)
+            void addNullPaddingToCallendarDays()
             {
-                f();
+                // Will add necessary number of nulls to the list so that the first non-null element's position
+                // matches that day's position in the calendar (monday - first, tuesday - second and so on)
+                // It would be nice to have a better way to add paddings but for know this is it
 
-                void f()
-                {
-                    if (shift.Date == enumerator.Current.Day)
-                        enumerator.Current.Shifts.Add(shift);
-                    else
-                    {
-                        enumerator.MoveNext();
-                        f();
-                    }
-                }
+                // DayOfWeek returns 0 for sunday, 1 for monday, 2 for tuesday and so on
+                int skips = (int)displayMonth.DayOfWeek - 1;
+                skips = (skips == -1) ? 6 : skips;
 
+                for (int i = 0; i < skips; i++)
+                    calendarDays.Add(null);
             }
 
-           
+            void addMonthDaysToCallendarDays(){
+                for (var day = displayMonth; day < displayMonth.AddMonths(1); day = day.AddDays(1))
+                    calendarDays.Add(new ScheduleDayViewModel(day));
+            }
 
-            ScheduleListBox.ItemsSource = days;
+            void matchScheduledShiftsToCallendarDays()
+            {
+                IEnumerator<ScheduleDayViewModel> daysOfMonth = calendarDays.GetEnumerator();
+                IEnumerator<ScheduledShift> scheduledShifts = scheduleService.GetScheduledShifts(
+                    start: displayMonth,
+                    end: displayMonth.AddMonths(1)
+                    ).GetEnumerator();
+
+                scheduledShifts.MoveNext();
+                while (scheduledShifts.Current != null)
+                {
+                    if (scheduledShifts.Current.Date == daysOfMonth?.Current?.Day)
+                    {
+                        daysOfMonth.Current.Shifts.Add(scheduledShifts.Current);
+                        scheduledShifts.MoveNext();
+                    }
+                    else
+                        daysOfMonth.MoveNext();
+
+                }
+            }
 
         }
         private void ScheduleTab_Loaded(object sender, RoutedEventArgs e)
@@ -112,6 +119,9 @@ namespace KreemMachine
 
         private void ScheduleListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (e.AddedItems.Count != 1)
+                return;
+
             var selected = e.AddedItems[0] as ScheduleDayViewModel;
 
             foreach(var shift in selected.Shifts)
