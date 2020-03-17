f7a8bbca1d1780eea702b8366ac789ab2ef5c4a0
Mihail Bondarenco <441044@student.fontys.nl>
2020-03-17 18:40:40 +0100

    feat(authorization) add permition system to security context

--- KreemMachine/KreemMachineLibrary/SecurityContext.cs
+++ KreemMachine/KreemMachineLibrary/SecurityContext.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static KreemMachineLibrary.Models.Role;
+using static KreemMachineLibrary.Permission;
 
 namespace KreemMachineLibrary
 {
@@ -11,9 +13,27 @@ namespace KreemMachineLibrary
     {
         public static User CurrentUser { get; private set; }
 
+        static readonly Dictionary<Role, Permission[]> PermissionTable = new Dictionary<Role, Permission[]> {
+            { Administrator, new[] { ViewUsers, CreateUsers, EditUsers, DeleteUsers, ViewSchedule, EditSchedule, AutogenerateSchedule, ViewStatistics, EditShifts,  } },
+            { Manager, new[] { ViewUsers, ViewSchedule, EditSchedule, AutogenerateSchedule, ViewStatistics, EditShifts,  } },
+            { Depot, new[] {ViewSchedule} },
+            { Employee, new Permission[]{} },
+        };
+
         internal static void Authenticate(User user)
         {
             CurrentUser = user;
         }
+
+        public static bool HasPermissions(params Permission[] permissions)
+        {
+            if (!CurrentUser.Role.HasValue) return false;
+
+            return PermissionTable[CurrentUser.Role.Value].Intersect(permissions).Count() == permissions.Length;
+        }
+
+        public static bool HasRole(Role? role) =>
+            CurrentUser.Role == role;
+        
     }
 }
