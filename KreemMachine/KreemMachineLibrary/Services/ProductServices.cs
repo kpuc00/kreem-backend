5fd82f3d31228248f6de403c334096a5b77fdc9d
Mihail Bondarenco <441044@student.fontys.nl>
2020-04-02 20:07:49 +0200

    feat(stock) add basic products page

---
+++ KreemMachine/KreemMachineLibrary/Services/ProductServices.cs
@@ -0,0 +1,20 @@
+﻿using KreemMachineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreemMachineLibrary.Services
+{
+    public class ProductServices
+    {
+        DataBaseContext db = Globals.db;
+
+        public List<Product> GetAllProducts()
+        {
+            return db.Products.ToList();
+        }
+
+    }
+}
