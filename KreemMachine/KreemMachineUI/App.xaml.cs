2ed49121f3b4246141d985a69c59fa6685c3a680
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-04-28 19:48:16 +0300

    Changed Display format to The Netherlands

--- KreemMachine/KreemMachineUI/App.xaml.cs
+++ KreemMachine/KreemMachineUI/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace KreemMachine
 {
@@ -13,5 +16,13 @@ namespace KreemMachine
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-NL");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-NL");
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+                        XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
     }
 }
